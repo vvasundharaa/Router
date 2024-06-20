0c54951fe8f44a2b5e68c6680c21d58d1e1e7159
Vasundhara <85097331+vvasundharaa@users.noreply.github.com>
2024-06-20 08:08:00 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import CollapsibleExample from './component/index';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CollapsibleExample/>
+    </div>
+  );
+}
+
+export default App;
