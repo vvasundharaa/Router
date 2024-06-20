4d6535ca0f406ca771fd40b8c8eff498f496f972
Vasundhara <85097331+vvasundharaa@users.noreply.github.com>
2024-06-20 15:27:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,337 @@
+export default Navbar;
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import backgroundImage from '../assets/performing-arts.jpg';
+import cardImg1 from '../assets/arts8.png';
+import cardImg2 from '../assets/culture1.jpg';
+import cardImg3 from '../assets/arts innovation.png';
+import './HomePage.css';
 
-function CollapsibleExample() {
+function HomePage() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
+    <div className="page-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="navbar-container m-5">
+        <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar-transparent">
+          <Container>
+             <Navbar.Brand as={Link} to="/">Shakticrafts.com</Navbar.Brand> 
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link className="navbar-link">Shakticrafts.com</Nav.Link>
+              </Nav>
+              <Nav className="ms-auto">
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/" className="navbar-link">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/about" className="navbar-link ms-5">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/arts" className="navbar-link ms-5">Arts</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/crafts" className="navbar-link ms-5">Crafts</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/culture" className="navbar-link ms-5">Culture</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/blogs" className="navbar-link ms-5">Blogs</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/contact" className="navbar-link ms-5">Contact</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div className="bottom-container">
+        <Container>
+          <p>"Explore the vibrant world of arts and culture, where creativity knows no bounds."</p>
+          <button className="bottom-button px-3 py-2">Explore</button>
+        </Container>
+      </div>
 
-export default CollapsibleExample;
+      <div className="cards-page">
+        <h1 className="card-heading">"Unleash your creativity: Dive into a world of Endless Arts and Crafts Possibilities!"</h1>
+        <div className="cards-container">
+          <div>
+            <img className="card-img" src={cardImg1} alt="Card Image 1" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+          <div>
+            <img className="card-img" src={cardImg2} alt="Card Image 2" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+          <div>
+            <img className="card-img" src={cardImg3} alt="Card Image 3" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+} 
+
+
+
+export default HomePage;export default Navbar;
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import backgroundImage from '../assets/performing-arts.jpg';
+import cardImg1 from '../assets/arts8.png';
+import cardImg2 from '../assets/culture1.jpg';
+import cardImg3 from '../assets/arts innovation.png';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="page-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="navbar-container m-5">
+        <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar-transparent">
+          <Container>
+             <Navbar.Brand as={Link} to="/">Shakticrafts.com</Navbar.Brand> 
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link className="navbar-link">Shakticrafts.com</Nav.Link>
+              </Nav>
+              <Nav className="ms-auto">
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/" className="navbar-link">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/about" className="navbar-link ms-5">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/arts" className="navbar-link ms-5">Arts</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/crafts" className="navbar-link ms-5">Crafts</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/culture" className="navbar-link ms-5">Culture</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/blogs" className="navbar-link ms-5">Blogs</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/contact" className="navbar-link ms-5">Contact</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div className="bottom-container">
+        <Container>
+          <p>"Explore the vibrant world of arts and culture, where creativity knows no bounds."</p>
+          <button className="bottom-button px-3 py-2">Explore</button>
+        </Container>
+      </div>
+
+      <div className="cards-page">
+        <h1 className="card-heading">"Unleash your creativity: Dive into a world of Endless Arts and Crafts Possibilities!"</h1>
+        <div className="cards-container">
+          <div>
+            <img className="card-img" src={cardImg1} alt="Card Image 1" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+          <div>
+            <img className="card-img" src={cardImg2} alt="Card Image 2" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+          <div>
+            <img className="card-img" src={cardImg3} alt="Card Image 3" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+} 
+
+
+
+export default HomePage;export default Navbar;
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import backgroundImage from '../assets/performing-arts.jpg';
+import cardImg1 from '../assets/arts8.png';
+import cardImg2 from '../assets/culture1.jpg';
+import cardImg3 from '../assets/arts innovation.png';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="page-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="navbar-container m-5">
+        <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar-transparent">
+          <Container>
+             <Navbar.Brand as={Link} to="/">Shakticrafts.com</Navbar.Brand> 
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link className="navbar-link">Shakticrafts.com</Nav.Link>
+              </Nav>
+              <Nav className="ms-auto">
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/" className="navbar-link">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/about" className="navbar-link ms-5">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/arts" className="navbar-link ms-5">Arts</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/crafts" className="navbar-link ms-5">Crafts</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/culture" className="navbar-link ms-5">Culture</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/blogs" className="navbar-link ms-5">Blogs</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/contact" className="navbar-link ms-5">Contact</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div className="bottom-container">
+        <Container>
+          <p>"Explore the vibrant world of arts and culture, where creativity knows no bounds."</p>
+          <button className="bottom-button px-3 py-2">Explore</button>
+        </Container>
+      </div>
+
+      <div className="cards-page">
+        <h1 className="card-heading">"Unleash your creativity: Dive into a world of Endless Arts and Crafts Possibilities!"</h1>
+        <div className="cards-container">
+          <div>
+            <img className="card-img" src={cardImg1} alt="Card Image 1" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+          <div>
+            <img className="card-img" src={cardImg2} alt="Card Image 2" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+          <div>
+            <img className="card-img" src={cardImg3} alt="Card Image 3" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+} 
+
+
+
+export default HomePage;export default Navbar;
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import backgroundImage from '../assets/performing-arts.jpg';
+import cardImg1 from '../assets/arts8.png';
+import cardImg2 from '../assets/culture1.jpg';
+import cardImg3 from '../assets/arts innovation.png';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="page-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="navbar-container m-5">
+        <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar-transparent">
+          <Container>
+             <Navbar.Brand as={Link} to="/">Shakticrafts.com</Navbar.Brand> 
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link className="navbar-link">Shakticrafts.com</Nav.Link>
+              </Nav>
+              <Nav className="ms-auto">
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/" className="navbar-link">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/about" className="navbar-link ms-5">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/arts" className="navbar-link ms-5">Arts</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/crafts" className="navbar-link ms-5">Crafts</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/culture" className="navbar-link ms-5">Culture</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/blogs" className="navbar-link ms-5">Blogs</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/contact" className="navbar-link ms-5">Contact</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div className="bottom-container">
+        <Container>
+          <p>"Explore the vibrant world of arts and culture, where creativity knows no bounds."</p>
+          <button className="bottom-button px-3 py-2">Explore</button>
+        </Container>
+      </div>
+
+      <div className="cards-page">
+        <h1 className="card-heading">"Unleash your creativity: Dive into a world of Endless Arts and Crafts Possibilities!"</h1>
+        <div className="cards-container">
+          <div>
+            <img className="card-img" src={cardImg1} alt="Card Image 1" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+          <div>
+            <img className="card-img" src={cardImg2} alt="Card Image 2" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+          <div>
+            <img className="card-img" src={cardImg3} alt="Card Image 3" />
+            <p>Card description</p>
+            <button>Button</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+} 
+
+
+
+export default HomePage;
