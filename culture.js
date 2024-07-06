47a5d0b2d8ea27d95f8e4dfcd8e4469e38ac0002
Vasundhara <85097331+vvasundharaa@users.noreply.github.com>
2024-07-06 17:56:23 +0530

    Create culture.js

---
+++ culture.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Culture.css';
+
+const CultureMainPage = () => {
+  return (
+    <div className='culture_img'> 
+      <div className='culture_content'>
+      <div className='culture_content_circle'>
+          <p className='circle_para'>
+            Culture is the invisible thread that weaves through every aspect of society, influencing our beliefs, customs, and identities. From ancient civilizations to modern-day societies, the diversity of cultures is a testament to the boundless creativity and resilience of humanity.
+          </p>
+        </div>
+        <div className='culture_content_square'>
+          <p >
+            Importance of Understanding Culture Understanding culture is crucial for fostering empathy, tolerance, and cross-cultural communication. It enables individuals to appreciate diversity, avoid misunderstandings, and build meaningful relationships with people from different backgrounds.
+          </p>
+        </div> 
+        </div>
+      </div>
+  );
+}
+
+export default CultureMainPage; 
+
+
+
